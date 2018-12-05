4fb0ed937048ee228fefb95b425cc65b39bf4a21
Reka <rekapap28@gmail.com>
2018-12-05 19:55:17 +0000

    adds hashwrapper set method

--- lib/hash_wrapper.rb
+++ lib/hash_wrapper.rb
@@ -1,6 +1,16 @@
 # Hash Wrapper
 class HashWrapper
+  def initialize
+    @data = {}
+  end
+
   def data
-    {}
+    @data.dup
+  end
+
+  def set(params)
+    params.each do |key, value|
+      @data[key] = value
+    end
   end
 end
--- spec/hash_wrapper_spec.rb
+++ spec/hash_wrapper_spec.rb
@@ -3,10 +3,17 @@ require 'hash_wrapper'
 describe HashWrapper do
   subject { described_class.new }
   describe '#data' do
-    context 'by defaault' do
+    context 'by default' do
       it 'returns a hash' do
         expect(subject.data).to be_a(Hash)
       end
+    end
+  end
+  describe '#set' do
+    let(:params) { { 'apple' => 'golden' } }
+    it 'set a pair into the data' do
+      subject.set(params)
+      expect(subject.data['apple']).to eq('golden')
     end
   end
 end
